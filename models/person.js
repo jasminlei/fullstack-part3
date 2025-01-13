b0571f3a1f3d7ae7eea96d185da6b1ab2d07d61b
jasminlei <jasminleinonen@hotmail.com>
2025-01-13 09:01:01 +0200

    teht 3.20

--- models/person.js
+++ models/person.js
@@ -14,7 +14,15 @@ mongoose
 
 const personSchema = new mongoose.Schema({
   name: { type: String, minlength: 3, required: true },
-  number: Number,
+  number: {
+    type: String,
+    minlength: 8,
+    validate: {
+      validator: function (v) {
+        return /^\d{2,3}-\d+$/.test(v);
+      },
+    },
+  },
 });
 
 personSchema.set("toJSON", {
